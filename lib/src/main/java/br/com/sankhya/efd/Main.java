34a72b522262a59163fdf61ad2a79b52eb3ae5eb
Tadeu.CS <tcssistemas2012@gmail.com>
2022-07-09 19:14:01 -0300

    criação de annotations para serem utilizadas nos fields e tratados na gravação dos registros no banco.

--- lib/src/main/java/br/com/sankhya/efd/Main.java
+++ lib/src/main/java/br/com/sankhya/efd/Main.java
@@ -1,9 +1,48 @@
 package br.com.sankhya.efd;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+
+import br.com.sankhya.efd.commons.utils.annotations.Column;
+import br.com.sankhya.efd.commons.utils.annotations.Validation;
+import br.com.sankhya.efd.icmsipi.models.registros.RegistroK100;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Olá nova estrutura de geração dos arquivos EFD!");
+		System.out.println("Olá nova estrutura de geração dos arquivos EFD!\n\n");
+
+		RegistroK100 regK100 = new RegistroK100();
+		regK100.setValorTotal(BigDecimal.valueOf(15.68));
+
+		try {
+			for (Field m : regK100.getClass().getDeclaredFields()) {
+				m.setAccessible(true);
+				String fieldName = m.getName();
+				System.out.println("Field: " + fieldName);
+				Object obj = m.get(regK100);
+				if(m.getType().equals(BigDecimal.class)) {
+					System.out.println("Value: " + ((BigDecimal) obj));
+				}else {
+					System.out.println(obj);
+				}
+				
+				for (Annotation annotation : m.getAnnotations()) {
+					if (annotation instanceof Column) {
+						Column column = (Column) annotation;
+						System.out.println("columnName:"+column.name());
+					}
+					if (annotation instanceof Validation) {
+						Validation validation = (Validation) annotation;
+						System.out.println("visible:" + validation.visible());
+						System.out.println("scale:" + validation.scale());
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- lib/src/main/java/br/com/sankhya/efd/icmsipi/models/registros/RegistroK100.java
+++ lib/src/main/java/br/com/sankhya/efd/icmsipi/models/registros/RegistroK100.java
@@ -1,9 +1,43 @@
 package br.com.sankhya.efd.icmsipi.models.registros;
 
+import java.math.BigDecimal;
+
+import br.com.sankhya.efd.commons.utils.annotations.Column;
+import br.com.sankhya.efd.commons.utils.annotations.Validation;
+
 public class RegistroK100 {
+	private BigDecimal valorDesconto;
+	private BigDecimal valorImposto;
+	@Column(name = "VALOR_TOTAL")
+	@Validation(scale = 6, visible = false)
+	private BigDecimal valorTotal;
 
 	public RegistroK100() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public BigDecimal getValorDesconto() {
+		return valorDesconto;
+	}
+
+	public void setValorDesconto(BigDecimal valorDesconto) {
+		this.valorDesconto = valorDesconto;
+	}
+
+	public BigDecimal getValorImposto() {
+		return valorImposto;
+	}
+
+	public void setValorImposto(BigDecimal valorImposto) {
+		this.valorImposto = valorImposto;
 	}
 
 }
